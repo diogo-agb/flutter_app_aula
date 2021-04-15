829c378f75554d01a1dbfed8da4eb287a4ea4b59
Diogo <diogo.agb@gmail.com>
2021-04-15 19:35:01 -0300

    increment change color

--- lib/main.dart
+++ lib/main.dart
@@ -13,52 +13,76 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter APP aula'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  var _color = Colors.blue;
+  String _txt = 'Par';
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+      parOuImpar(_counter);
     });
+  }
+
+  void parOuImpar(int counter) {
+    if (counter % 2 == 0) {
+      _color = Colors.blue;
+      _txt = 'Par';
+    } else {
+      _color = Colors.red;
+      _txt = 'Ímpar';
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    print('Diogo');
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('APP'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        color: _color,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // buildDropdownButton(),
+              Text(
+                'Aperte o botão para incrementar o número e mudar de cor!',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                '$_counter',
+                style: TextStyle(fontSize: 50),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '$_txt',
+                style: TextStyle(fontSize: 40),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: _color,
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
